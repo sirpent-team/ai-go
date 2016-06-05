f6bb28dd2431f32daeeaa164db793b9f1fe4d19f
Michael Mokrysz <hi@46b.it>
2016-06-05 14:39:34 +0100

    Fixed missing Snake HasSegmentAt test.

--- snake_test.go
+++ snake_test.go
@@ -56,19 +56,27 @@ func TestHasCollidedIntoSnake(t *testing.T) {
 	s1.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 1}}
 
 	s2 := sirpent.Snake{Length: 3, Dead: false, Segments: make([]sirpent.SnakeSegment, 3)} // No collision with s1
-	s2.Segments[0] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 0, Y: 0}}
-	s2.Segments[1] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 0}}
-	s2.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 1}}
+	s2.Segments[0] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 5, Y: 5}}
+	s2.Segments[1] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 6, Y: 5}}
+	s2.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 6, Y: 6}}
 
-	s3 := sirpent.Snake{Length: 3, Dead: false, Segments: make([]sirpent.SnakeSegment, 3)} // Head collided with s1
+	s3 := sirpent.Snake{Length: 3, Dead: false, Segments: make([]sirpent.SnakeSegment, 3)} // s1 collides with head
 	s3.Segments[0] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 0, Y: 0}}
-	s3.Segments[1] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 0}}
-	s3.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 1}}
+	s3.Segments[1] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: -1, Y: 0}}
+	s3.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: -2, Y: 0}}
 
-	s4 := sirpent.Snake{Length: 3, Dead: false, Segments: make([]sirpent.SnakeSegment, 3)} // Body collided with s1
-	s4.Segments[0] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 0, Y: 0}}
-	s4.Segments[1] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 0}}
-	s4.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 1, Y: 1}}
+	s4 := sirpent.Snake{Length: 3, Dead: false, Segments: make([]sirpent.SnakeSegment, 3)} // s1 collides with body
+	s4.Segments[0] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 0, Y: 1}}
+	s4.Segments[1] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 0, Y: 0}}
+	s4.Segments[2] = sirpent.SnakeSegment{Position: sirpent.HexagonalVector{X: 0, Y: -1}}
 
-	s1.HasCollidedIntoSnake(s2)
+	if s1.HasCollidedIntoSnake(s2) {
+		t.Error("Incorrect collision with snake s2.")
+	}
+	if !s1.HasCollidedIntoSnake(s3) {
+		t.Error("Unnoticed head collision with snake s3.")
+	}
+	if !s1.HasCollidedIntoSnake(s4) {
+		t.Error("Unnoticed body collision with snake s4.")
+	}
 }
